54a1dd8f72f048b0e2640424941d57ef3a1fd60d
GeorgiTsukanov <georgicukanov@gmail.com>
2026-05-18 21:42:19 +0000

    Add username input functionality

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+#include <string>
 
 using namespace std; //загрязнение глобального пространства имен
+
 int main() {
     cout << "Hello world" << endl;
+
+    string name;
+    cout << "Enter your name: ";
+    cin >> name;
+    cout << "Hello world from @" << name << endl;
     return 0;
 }
+
+
+
